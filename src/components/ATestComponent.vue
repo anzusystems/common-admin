afe6365514607c1e43e49d1834a930dcb6875458
sakulb <lukas.budos@gmail.com>
2023-01-12 18:30:57 +0100

    Test comp.

---
+++ src/components/ATestComponent.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup>
+import ABooleanValue from '@/components/ABooleanValue.vue'
+</script>
+
+<template>
+  Test: <ABooleanValue :value="true" chip></ABooleanValue>
+</template>
