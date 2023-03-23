1cee247cbca7bf7395971c1507f2b07e1306209d
volar <stanislav.volar@petitpress.sk>
2023-03-23 17:47:46 +0100

    added placeholder for string filter

--- src/components/filter/AFilterString.vue
+++ src/components/filter/AFilterString.vue
@@ -3,13 +3,16 @@ import type { Filter } from '@/types/Filter'
 import { useFilterHelpers } from '@/composables/filter/filterHelpers'
 import { computed } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { isUndefined } from '@/utils/common'
 
 const props = withDefaults(
   defineProps<{
     modelValue: Filter
+    placeholder?: string | undefined
     dataCy?: string
   }>(),
   {
+    placeholder: undefined,
     dataCy: undefined,
   }
 )
@@ -33,12 +36,20 @@ const { t } = useI18n()
 const label = computed(() => {
   return props.modelValue.titleT ? t(props.modelValue.titleT) : undefined
 })
+
+const placeholderComputed = computed(() => {
+  if (!isUndefined(props.placeholder)) return props.placeholder
+  if (props.modelValue.variant === 'startsWith') return t('common.model.filterPlaceholder.startsWith')
+  if (props.modelValue.variant === 'eq') return t('common.model.filterPlaceholder.eq')
+  return t('common.model.filterPlaceholder.anyWhere')
+})
 </script>
 
 <template>
   <VTextField
     v-model="value"
     :label="label"
+    :placeholder="placeholderComputed"
     :clearable="!modelValue.mandatory"
     :data-cy="dataCy"
     @click:clear.stop="clearOne(modelValue)"
