d1e1941d0875fecb92130866787686aaced0aa71
volar <stanislav.volar@petitpress.sk>
2023-07-07 23:30:05 +0200

    image wip

---
+++ src/playground/image/ImageView.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import ActionbarWrapper from '@/playground/system/ActionbarWrapper.vue'
+import AImageSimple from '@/components/image/AImageSimple.vue'
+import AImage from '@/components/image/AImage.vue'
+</script>
+
+<template>
+  <ActionbarWrapper />
+
+  <VCard>
+    <VCardTitle>Image</VCardTitle>
+    <VCardText>
+      <VRow>
+        <VCol cols="4">
+          <AImageSimple />
+        </VCol>
+      </VRow>
+      <VRow>
+        <VCol cols="4">
+          <AImage />
+        </VCol>
+      </VRow>
+    </VCardText>
+  </VCard>
+</template>
