7249893466b3e4f81408c9a199fddbdcc0f304dd
Stanislav Volar <stanislav.volar@gmail.com>
2023-08-18 10:09:26 +0200

    wip (#139)

---
+++ src/playground/alertView/AlertView.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import ActionbarWrapper from '@/playground/system/ActionbarWrapper.vue'
+import { useAlerts } from '@/composables/system/alerts'
+
+const { showError } = useAlerts()
+const alert = () => {
+  showError('Error test')
+}
+</script>
+
+<template>
+  <ActionbarWrapper />
+
+  <VCard>
+    <VCardTitle>Alert</VCardTitle>
+    <VCardText>
+      <VRow>
+        <VCol>
+          <VBtn @click.stop="alert">
+            Test
+          </VBtn>
+        </VCol>
+      </VRow>
+    </VCardText>
+  </VCard>
+</template>
