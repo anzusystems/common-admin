e7d0db59bd247e7aaca49a56ca7ca32925015003
sakulb <lukas.budos@gmail.com>
2024-01-22 16:10:52 +0100

    Add priority and scheduledAt to jobs.

--- src/components/job/AJobBaseCreateForm.vue
+++ src/components/job/AJobBaseCreateForm.vue
@@ -7,24 +7,10 @@ import ARow from '@/components/ARow.vue'
 import AFormDatetimePicker from '@/components/form/AFormDatetimePicker.vue'
 import { useI18n } from 'vue-i18n'
 
-const props = defineProps<{
-  modelValue: JobBase
-}>()
-
-const emit = defineEmits<{
-  (e: 'update:modelValue', data: JobBase): void
-}>()
-const job = computed({
-  get() {
-    return props.modelValue
-  },
-  set(newValue: JobBase) {
-    emit('update:modelValue', newValue)
-  },
-})
+const modelValue = defineModel<JobBase>({ required: true })
 const { priorityLabels, getPriorityColor } = useJobPriority()
 const sliderColor = computed(() => {
-  return getPriorityColor(job.value.priority)
+  return getPriorityColor(modelValue.value.priority)
 })
 const { t } = useI18n()
 </script>
@@ -32,13 +18,13 @@ const { t } = useI18n()
 <template>
   <ARow>
     <AFormDatetimePicker
-      v-model="job.scheduledAt"
+      v-model="modelValue.scheduledAt"
       :label="t('common.job.model.scheduledAt')"
     />
   </ARow>
   <ARow>
     <VSlider
-      v-model="job.priority"
+      v-model="modelValue.priority"
       :ticks="priorityLabels"
       :max="2"
       step="1"
