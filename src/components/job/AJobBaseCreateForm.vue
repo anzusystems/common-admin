a99b417ff87186d39be8fc1c89362eb3d796fd43
sakulb <lukas.budos@gmail.com>
2024-01-22 16:01:20 +0100

    Add priority and scheduledAt to jobs.

--- src/components/job/AJobBaseCreateForm.vue
+++ src/components/job/AJobBaseCreateForm.vue
@@ -5,6 +5,7 @@ import type { JobBase } from '@/types/Job'
 import { useJobPriority } from '@/components/job/composables/jobPriority'
 import ARow from '@/components/ARow.vue'
 import AFormDatetimePicker from '@/components/form/AFormDatetimePicker.vue'
+import { useI18n } from 'vue-i18n'
 
 const props = defineProps<{
   modelValue: JobBase
@@ -25,13 +26,14 @@ const { priorityLabels, getPriorityColor } = useJobPriority()
 const sliderColor = computed(() => {
   return getPriorityColor(job.value.priority)
 })
+const { t } = useI18n()
 </script>
 
 <template>
   <ARow>
     <AFormDatetimePicker
       v-model="job.scheduledAt"
-      label="tScheduledAt"
+      :label="t('common.job.model.scheduledAt')"
     />
   </ARow>
   <ARow>
