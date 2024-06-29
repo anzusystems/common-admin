992bc49dec45aa06f37bc30934f59c545ce1f46b
volar <stanislav.volar@petitpress.sk>
2024-06-29 13:32:25 +0200

    fix Acl.vue

--- src/components/permission/Acl.vue
+++ src/components/permission/Acl.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup generic="TAclValue extends AclValue">
-import { ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
 import type { AclValue } from '@/types/Permission'
 import { defineAuth } from '@/composables/auth/defineAuth'
 import { isUndefined } from '@/utils/common'
@@ -14,30 +14,30 @@ const props = withDefaults(
   }
 )
 
-const getSystemFromAcl = (acl: TAclValue) => {
-  const parts = acl.split('_')
-  return parts[0]
-}
-
-// eslint-disable-next-line vue/no-setup-props-reactivity-loss
-const { can, useCurrentUser } = defineAuth<AclValue>(getSystemFromAcl(props.permission))
-// eslint-disable-next-line vue/no-setup-props-reactivity-loss
-const { currentUser } = useCurrentUser(getSystemFromAcl(props.permission))
-
-// eslint-disable-next-line vue/no-setup-props-reactivity-loss
 const allowed = ref<boolean>(false)
 
-watch(
-  currentUser,
-  (newValue) => {
-    if (!isUndefined(newValue)) {
-      allowed.value = can(props.permission, props.subject)
+const system = computed(() => {
+  const parts = props.permission.split('_')
+  return parts[0] || ''
+})
+
+onMounted(() => {
+  const { can, useCurrentUser } = defineAuth<AclValue>(system.value)
+
+  const { currentUser } = useCurrentUser(system.value)
+
+  watch(
+    currentUser,
+    (newValue) => {
+      if (!isUndefined(newValue)) {
+        allowed.value = can(props.permission, props.subject)
+      }
+    },
+    {
+      immediate: true,
     }
-  },
-  {
-    immediate: true,
-  }
-)
+  )
+})
 </script>
 
 <template>
