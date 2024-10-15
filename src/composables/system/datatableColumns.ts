359c1330dc11fb80847a0b0dff585a6e5612b6c8
volar <stanislav.volar@petitpress.sk>
2024-10-15 10:19:01 +0200

    fix

--- src/composables/system/datatableColumns.ts
+++ src/composables/system/datatableColumns.ts
@@ -88,7 +88,7 @@ export function createDatatableColumnsConfig(
     return columns
   })
 
-  const updateSortBy = (sortBy: { key: string, order: 'asc' | 'desc' } | undefined) => {
+  const updateSortBy = (sortBy: { key: string, order: 'asc' | 'desc' } | undefined | null) => {
     if (sortBy) {
       pagination.sortBy = sortBy.key
       pagination.descending = sortBy.order === 'desc' ? true : false
