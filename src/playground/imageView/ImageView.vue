7fbcde9a923b60695e6f5bdde6eff0b674dc9108
volar <stanislav.volar@petitpress.sk>
2025-03-19 20:48:27 +0100

    copy to licence wip

--- src/playground/imageView/ImageView.vue
+++ src/playground/imageView/ImageView.vue
@@ -130,7 +130,7 @@ const saveInsideDialog = () => {
           <AImageMediaWidget
             v-model:image="imageId4"
             v-model:media="media"
-            :upload-licence="100000"
+            :upload-licence="100001"
             :select-licences="[100000, 100001]"
             queue-key="media"
             label="Priority content"
