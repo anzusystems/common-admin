3097c0b23b0b5ef6424a8abc5843e199b078d2f2
volar <stanislav.volar@petitpress.sk>
2025-03-28 23:28:37 +0100

    update deps

--- src/components/filter2/FilterBookmarks.vue
+++ src/components/filter2/FilterBookmarks.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue'
+import { computed, nextTick, onMounted, ref, useTemplateRef } from 'vue'
 import { useFilterBookmarkStore } from '@/components/filter2/bookmarksStore.ts'
 import { useUserAdminConfigApi } from '@/services/api/userAdminConfig/userAdminConfig.ts'
 import type { AxiosInstance } from 'axios'
 import { type UserAdminConfig, UserAdminConfigLayoutType } from '@/types/UserAdminConfig.ts'
 import { useDisplay } from 'vuetify'
 import type { IntegerId } from '@/types/common.ts'
+import { useResizeObserver, watchThrottled } from '@vueuse/core'
+import { isNull } from '@/utils/common.ts'
 
 const props = withDefaults(
   defineProps<{
@@ -24,6 +26,10 @@ const datatableHiddenColumns = defineModel<string[] | undefined>('datatableHidde
 
 const items = ref<UserAdminConfig[]>([])
 const loading = ref(false)
+const toolbarRef = useTemplateRef('toolbarRef')
+const toolbarWidth = ref(300)
+const visibleItemsCount = ref(1000)
+
 const filterBookmarkStore = useFilterBookmarkStore()
 // eslint-disable-next-line vue/no-setup-props-reactivity-loss
 const { fetchUserAdminConfigList } = useUserAdminConfigApi(props.client, props.system)
@@ -49,19 +55,103 @@ const onItemClick = (item: UserAdminConfig) => {
   console.log(datatableHiddenColumns.value)
 }
 
+const THREE_DOTS_WIDTH = 32
+
+const calculateVisible = async (totalWidth: number) => {
+  const container = toolbarRef.value
+  if (isNull(container)) return
+  visibleItemsCount.value = 1000
+  await nextTick()
+  let cumulativeWidth = 0
+  let visible = 0
+  for (let i = 0; i < container.children.length; i++) {
+    const child = container.children[i] as HTMLElement
+    const childWidth = child.offsetWidth
+    cumulativeWidth += childWidth
+    if (cumulativeWidth >= totalWidth - THREE_DOTS_WIDTH) {
+      break
+    }
+    visible++
+  }
+  visibleItemsCount.value = visible
+}
+
+useResizeObserver(toolbarRef, (entries) => {
+  if (entries[0]) {
+    toolbarWidth.value = entries[0].contentRect.width
+  }
+})
+
+watchThrottled(
+  toolbarWidth,
+  (newValue, oldValue) => {
+    if (newValue === oldValue) return
+    calculateVisible(newValue)
+  },
+  { throttle: 1000 }
+)
+
+const itemsComputed = computed(() => {
+  const visible = visibleItemsCount.value
+  return {
+    visible: items.value.slice(0, visible),
+    hidden: items.value.slice(visible),
+  }
+})
+
+nextTick(() => {
+  if (toolbarRef.value) {
+    toolbarWidth.value = toolbarRef.value.clientWidth
+  }
+})
+
 onMounted(() => {
   loadBookmarks()
 })
 </script>
 
 <template>
-  <VBtn
-    v-for="item in items"
-    :key="item.id"
-    size="small"
-    text
-    @click.stop="onItemClick(item)"
+  <div
+    ref="toolbarRef"
+    class="w-100 d-flex overflow-hidden"
   >
-    {{ item.customName }}
-  </VBtn>
+    <div v-if="loading" />
+    <template v-else>
+      <div
+        v-for="item in itemsComputed.visible"
+        :key="item.id"
+        class="white-space-nowrap"
+      >
+        <VBtn
+          text
+          size="small"
+          @click="onItemClick(item)"
+        >
+          {{ item.customName }}
+        </VBtn>
+      </div>
+      <VMenu
+        v-if="itemsComputed.hidden.length > 0"
+        bottom
+        offset-y
+      >
+        <template #activator="{ props: activatorProps }">
+          <VBtn
+            icon="mdi-menu-down"
+            size="small"
+            v-bind="activatorProps"
+          />
+        </template>
+        <VList density="compact">
+          <VListItem
+            v-for="item in itemsComputed.hidden"
+            :key="item.id"
+            @click="onItemClick(item)"
+          >
+            <VListItemTitle>{{ item.customName }}</VListItemTitle>
+          </VListItem>
+        </VList>
+      </VMenu>
+    </template>
+  </div>
 </template>
