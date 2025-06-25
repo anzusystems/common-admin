1ca7cca494f3fdf88973e97232aba307eec9ca20
volar <stanislav.volar@petitpress.sk>
2025-06-25 13:56:59 +0200

    remove deprecated plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
-import { splitVendorChunkPlugin } from 'vite'
 
 const _dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [
-    splitVendorChunkPlugin(),
     vue(),
     vuetify({
       autoImport: true,
