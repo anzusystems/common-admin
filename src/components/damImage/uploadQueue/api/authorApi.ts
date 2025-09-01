d59354479e5648c15facd495c7a325574f7a0084
volar <stanislav.volar@petitpress.sk>
2025-09-01 22:12:41 +0200

    wip

--- src/components/damImage/uploadQueue/api/authorApi.ts
+++ src/components/damImage/uploadQueue/api/authorApi.ts
@@ -3,10 +3,6 @@ import { apiFetchByIds } from '@/services/api/apiFetchByIds'
 import type { DamAuthor } from '@/components/damImage/uploadQueue/author/DamAuthor'
 import { SYSTEM_CORE_DAM } from '@/components/damImage/uploadQueue/api/damAssetApi'
 import { apiCreateOne } from '@/services/api/apiCreateOne'
-import { apiFetchList } from '@/services/api/apiFetchList'
-import type { Pagination } from '@/types/Pagination'
-import type { FilterBag } from '@/types/Filter'
-import { cmsClient } from '@/playground/mock/cmsClient'
 import { useApiFetchList } from '@/labs/api/useApiFetchList'
 import type { IntegerId } from '@/types/common'
 
@@ -30,24 +26,6 @@ export const fetchAuthorListByIds = (client: () => AxiosInstance, extSystemId: n
 
 export const useFetchAuthorList = (client: () => AxiosInstance, extSystemId: IntegerId) =>
   useApiFetchList<DamAuthor[]>(client, SYSTEM_CORE_DAM, ENTITY, END_POINT_LIST, { extSystemId })
-
-export const fetchAuthorList = (
-  client: () => AxiosInstance,
-  extSystemId: number,
-  pagination: Pagination,
-  filterBag: FilterBag
-) =>
-  apiFetchList<DamAuthor[]>(
-    client,
-    END_POINT_LIST,
-    {
-      extSystemId,
-    },
-    pagination,
-    filterBag,
-    SYSTEM_CORE_DAM,
-    ENTITY
-  )
 
 export const createAuthor = (client: () => AxiosInstance, data: DamAuthor) =>
   apiCreateOne<DamAuthor>(client, data, END_POINT, {}, SYSTEM_CORE_DAM, ENTITY)
