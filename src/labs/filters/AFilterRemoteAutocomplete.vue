35d34d7b273eeb0222376dbd7933bbdfef0843df
volar <stanislav.volar@petitpress.sk>
2025-09-19 11:53:14 +0200

    refactor AFilterRemoteAutocomplete according to new AFormRemoteAutocomplete

--- src/labs/filters/AFilterRemoteAutocomplete.vue
+++ src/labs/filters/AFilterRemoteAutocomplete.vue
@@ -1,10 +1,10 @@
-<script lang="ts" setup>
+<script lang="ts" setup generic="T extends string | number">
 import { watchDebounced } from '@vueuse/core'
 import { computed, getCurrentInstance, inject, type Ref, ref, watch } from 'vue'
 import type { ValueObjectOption } from '@/types/ValueObject'
-import { isArray, isBoolean, isNull, isUndefined } from '@/utils/common'
+import { isArray, isBoolean, isEmpty, isNull, isUndefined } from '@/utils/common'
 import { useI18n } from 'vue-i18n'
-import type { DocId, IntegerId } from '@/types/common'
+import { type FilterConfig, type FilterData, useFilterClearHelpers } from '@/labs/filters/filterFactory'
 import {
   FilterConfigKey,
   FilterDataKey,
@@ -13,15 +13,11 @@ import {
   FilterSelectedKey,
   FilterSubmitResetCounterKey,
 } from '@/labs/filters/filterInjectionKeys'
-import { type FilterConfig, type FilterData, useFilterClearHelpers } from '@/labs/filters/filterFactory'
-import { isOneOf } from '@/utils/enum'
 import type { DatatableSortBy } from '@/composables/system/datatableColumns'
 import { type Pagination, usePagination } from '@/labs/filters/pagination'
 import { useAlerts } from '@/composables/system/alerts'
 
-type FetchItemsByIdsType =
-  | ((ids: IntegerId[]) => Promise<ValueObjectOption<IntegerId>[]>)
-  | ((ids: DocId[]) => Promise<ValueObjectOption<DocId>[]>)
+type ModelValueType = T | T[] | null | undefined
 
 const props = withDefaults(
   defineProps<{
@@ -30,19 +26,21 @@ const props = withDefaults(
       pagination: Ref<Pagination>,
       filterData: FilterData,
       filterConfig: FilterConfig
-    ) => Promise<ValueObjectOption<string | number>[]>
-    fetchItemsByIds: FetchItemsByIdsType
+    ) => Promise<ValueObjectOption<T>[]>
+    fetchItemsByIds: (ids: T[]) => Promise<ValueObjectOption<T>[]>
     filterByField: string
     filterSortBy?: DatatableSortBy
-    placeholder?: string | undefined
     prefetch?: 'hover' | 'focus' | 'mounted' | false
-    searchMinChars?: number
+    minSearchChars?: number
+    minSearchText?: string | undefined
+    placeholder?: string | undefined
   }>(),
   {
     filterSortBy: null,
-    placeholder: undefined,
     prefetch: false,
-    searchMinChars: 2,
+    minSearchChars: 2,
+    minSearchText: undefined,
+    placeholder: undefined,
   }
 )
 const emit = defineEmits<{
@@ -69,134 +67,131 @@ if (
   throw new Error(`[${componentName}] Incorrect provide/inject config.`)
 }
 
-if (
-  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
-  isUndefined(filterConfig.fields[props.name]) ||
-  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
-  isUndefined(filterData[props.name])
-) {
+// eslint-disable-next-line vue/no-setup-props-reactivity-loss
+const filterByFieldProp = props.filterByField
+// eslint-disable-next-line vue/no-setup-props-reactivity-loss
+const filterSortByProp = props.filterSortBy
+
+if (isUndefined(filterInnerConfig.fields[filterByFieldProp]) || isUndefined(filterInnerData[filterByFieldProp])) {
   throw new Error(
-    `[${componentName}] Incorrect filter config. ` +
-      `Name is '${props.name}' and available options are ${Object.keys(filterData).join(', ')}.`
+    `[${componentName}] Incorrect filter inner config. ` +
+      `FilterByField is '${filterByFieldProp}' and available options are ${Object.keys(filterInnerData).join(', ')}.`
   )
 }
 
-if (
-  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
-  isUndefined(filterInnerConfig.fields[props.filterByField]) ||
-  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
-  isUndefined(filterInnerData[props.filterByField])
-) {
-  throw new Error(
-    `[${componentName}] Incorrect filter inner config. ` +
-      `FilterByField is '${props.filterByField}' and available options are ${Object.keys(filterInnerData).join(', ')}.`
-  )
-}
+const filterConfigCurrent = computed(() => filterConfig.fields[props.name])
+
+const SEARCH_DEBOUNCE_MS = 300
 
 const selected = ref<any>([])
 const search = ref('')
 const isFocused = ref(false)
-const autoFetchTimer: Ref<ReturnType<typeof setTimeout> | undefined> = ref(undefined)
-
+const modelValueAutocomplete = ref<ValueObjectOption<T> | ValueObjectOption<T>[] | null>(null)
 const apiRequestCounter = ref(0)
 
 const { t } = useI18n()
 
-const filterConfigCurrent = computed(() => filterConfig.fields[props.name])
-
 const onBlur = () => {
-  isFocused.value = true
+  isFocused.value = false
 }
 
 const label = computed(() => {
   return filterConfigCurrent.value.titleT ? t(filterConfigCurrent.value.titleT) : undefined
 })
 
-// eslint-disable-next-line vue/no-setup-props-reactivity-loss
-const { pagination } = usePagination(
-  // eslint-disable-next-line vue/no-setup-props-reactivity-loss
-  isNull(props.filterSortBy) ? null : props.filterSortBy.key,
-  props.filterSortBy?.order
-)
-const fetchedItems = ref<ValueObjectOption<string | number>[]>([])
-const selectedItemsCache = ref<ValueObjectOption<string | number>[]>([])
+const { pagination } = usePagination(isNull(filterSortByProp) ? null : filterSortByProp.key, filterSortByProp?.order)
 
-const allItems = computed<ValueObjectOption<DocId | IntegerId>[]>(() => {
-  const final = new Map()
-  selectedItemsCache.value.forEach((value) => {
-    final.set(value.value, { value: value.value, title: value.title, subtitle: value.subtitle })
-  })
-  fetchedItems.value.forEach((value) => {
-    final.set(value.value, { value: value.value, title: value.title, subtitle: value.subtitle })
-  })
-  return Array.from(final, ([key, value]) => {
-    return { value: key, title: value.title, subtitle: value.subtitle }
-  })
+const fetchedItems = ref<ValueObjectOption<T>[]>([])
+const selectedItemsCache = ref<ValueObjectOption<T>[]>([])
+const isFirstLoad = ref(true)
+const prefetchCompleted = ref(false)
+
+const allItems = computed<ValueObjectOption<T>[]>(() => {
+  const itemsMap = new Map<T, ValueObjectOption<T>>()
+  const addToMap = (items: ValueObjectOption<T>[]) => {
+    items.forEach((item) => {
+      itemsMap.set(item.value, {
+        value: item.value,
+        title: item.title,
+        subtitle: item.subtitle,
+      } as ValueObjectOption<T>)
+    })
+  }
+
+  addToMap(selectedItemsCache.value as ValueObjectOption<T>[])
+  addToMap(fetchedItems.value as ValueObjectOption<T>[])
+
+  return Array.from(itemsMap.values())
 })
 
 const loading = ref(false)
+
+const resetToEmptyState = (value: ModelValueType) => {
+  selectedItemsCache.value = []
+  selected.value = isArray(value) ? [] : null
+  modelValueAutocomplete.value = isArray(value) ? [] : null
+}
+
+const updateSelected = (value: T[] | T) => {
+  const findItem = (id: T): ValueObjectOption<T> =>
+    allItems.value.find((obj) => obj.value === id) ?? ({ title: `${id}`, value: id } as ValueObjectOption<T>)
+  return isArray(value) ? value.map(findItem) : findItem(value)
+}
+
+const loadListItems = async (ids: T[] | T) => {
+  loading.value = true
+
+  try {
+    const idsArray = isArray(ids) ? ids : [ids]
+    selectedItemsCache.value = await props.fetchItemsByIds(idsArray)
+    const selectedNewValue = updateSelected(ids)
+    selected.value = selectedNewValue
+    modelValueAutocomplete.value = selectedNewValue
+    return selectedItemsCache.value
+  } finally {
+    loading.value = false
+  }
+}
 
 const { showErrorsDefault } = useAlerts()
 
 const apiSearch = async (query: string, requestCounter: number) => {
   loading.value = true
-  filterInnerData[props.filterByField] = query
+  filterInnerData[filterByFieldProp] = query
   try {
     const res = await props.fetchItems(pagination, filterInnerData, filterInnerConfig)
     if (requestCounter === apiRequestCounter.value) fetchedItems.value = res
+  } catch (error) {
+    showErrorsDefault(error)
+  } finally {
     loading.value = false
-  } catch (e) {
-    loading.value = false
-    showErrorsDefault(e)
   }
 }
 
-const findLocalDataByValues = (values: Array<DocId | IntegerId>) => {
-  const found = allItems.value.filter((item: ValueObjectOption<string | number>) => values.includes(item.value))
-  return ([] as ValueObjectOption<string | number>[]).concat(found)
-}
+const tryAutoFetch = async (mode: 'focus' | 'hover' | 'mounted') => {
+  if (props.prefetch === false || props.prefetch !== mode) return
+  if (loading.value) return
+  if (prefetchCompleted.value) return
 
-const tryToLoadFromLocalData = async (newValue: string | number | Array<string | number>) => {
-  let count = 1
-  let foundItems: ValueObjectOption<string | number>[] = []
-  if (isArray(newValue)) {
-    count = newValue.length
-    foundItems = findLocalDataByValues(newValue)
-  } else {
-    foundItems = findLocalDataByValues([newValue])
-  }
-  selectedItemsCache.value = foundItems
-  return foundItems.length === count
-}
-
-const autoFetched = ref(false)
-
-const clearAutoFetchTimer = () => {
-  clearTimeout(autoFetchTimer.value)
-  autoFetchTimer.value = undefined
-}
-
-const autoFetch = async () => {
-  clearAutoFetchTimer()
-  if (autoFetched.value === true) return
-  autoFetched.value = true
   loading.value = true
-  const res = await props.fetchItems(pagination, filterInnerData, filterInnerConfig)
-  if (apiRequestCounter.value === 0) fetchedItems.value = res
-  loading.value = false
+  try {
+    const res = await props.fetchItems(pagination, filterInnerData, filterInnerConfig)
+    if (apiRequestCounter.value === 0) {
+      fetchedItems.value = res
+    }
+    prefetchCompleted.value = true
+  } finally {
+    loading.value = false
+  }
 }
 
-const onFocus = () => {
+const onFocus = async () => {
   isFocused.value = true
-  if (props.prefetch === false) return
-  clearAutoFetchTimer()
-  autoFetch()
+  await tryAutoFetch('focus')
 }
 
-const onMouseEnter = () => {
-  if (props.prefetch === 'focus' || props.prefetch === false) return
-  clearAutoFetchTimer()
-  autoFetch()
+const onMouseEnter = async () => {
+  await tryAutoFetch('hover')
 }
 
 const singleItemSelectedTitle = computed(() => {
@@ -212,6 +207,54 @@ const onSearchUpdate = (query: string) => {
   search.value = query
 }
 
+const { clearOne } = useFilterClearHelpers()
+
+const clearField = () => {
+  clearOne(props.name, filterData, filterConfig)
+  filterSelected.value.delete(props.name)
+}
+
+const onClickClear = async () => {
+  fetchedItems.value = await props.fetchItems(pagination, filterInnerData, filterInnerConfig)
+  clearField()
+}
+
+watchDebounced(
+  search,
+  (newValue, oldValue) => {
+    if (newValue.length < props.minSearchChars) return
+    if (newValue !== oldValue) {
+      apiRequestCounter.value++
+      apiSearch(newValue, apiRequestCounter.value)
+    }
+  },
+  { debounce: SEARCH_DEBOUNCE_MS }
+)
+
+const checkFirstLoad = async () => {
+  await tryAutoFetch('mounted')
+}
+
+const placeholderComputed = computed(() => {
+  if (!isUndefined(props.placeholder)) return props.placeholder
+  if (filterConfigCurrent.value.variant === 'startsWith') return t('common.model.filterPlaceholder.startsWith')
+  if (filterConfigCurrent.value.variant === 'eq') return t('common.model.filterPlaceholder.eq')
+  if (filterConfigCurrent.value.variant === 'search') return t('common.model.filterPlaceholder.contains')
+  return ''
+})
+
+const noDataText = computed(() => {
+  if (loading.value) {
+    return '$vuetify.loading'
+  }
+  if (fetchedItems.value.length === 0 && search.value.length < props.minSearchChars) {
+    return isUndefined(props.minSearchText)
+      ? t('common.filter.filterMinChars', { min: props.minSearchChars })
+      : props.minSearchText
+  }
+  return undefined
+})
+
 const onSelectedUpdate = (newValue: any) => {
   filterConfig.touched = true
   let final: null | string | number | string[] | number[] = null
@@ -224,95 +267,22 @@ const onSelectedUpdate = (newValue: any) => {
   emit('change')
 }
 
-const onClickClear = async () => {
-  fetchedItems.value = await props.fetchItems(pagination, filterInnerData, filterInnerConfig)
-  clearField()
-}
-
-const placeholderComputed = computed(() => {
-  if (!isUndefined(props.placeholder)) return props.placeholder
-  if (filterConfigCurrent.value.variant === 'startsWith') return t('common.model.filterPlaceholder.startsWith')
-  if (filterConfigCurrent.value.variant === 'eq') return t('common.model.filterPlaceholder.eq')
-  if (filterConfigCurrent.value.variant === 'search') return t('common.model.filterPlaceholder.contains')
-  return ''
-})
-
-const { clearOne } = useFilterClearHelpers()
-
-const clearField = () => {
-  clearOne(props.name, filterData, filterConfig)
-  filterSelected.value.delete(props.name)
-}
-
-const updateFilterSelected = (
-  newValue: ValueObjectOption<string | number> | ValueObjectOption<string | number>[] | null
-) => {
-  filterSelected.value.delete(props.name)
-  if ((isArray(newValue) && newValue.length === 0) || isNull(newValue)) {
-    filterSelected.value.delete(props.name)
-    return
-  }
-  if (isArray(newValue)) {
-    filterSelected.value.set(
-      props.name,
-      newValue.map((item) => ({ title: item.title, value: item.value }))
-    )
-    return
-  }
-  filterSelected.value.set(props.name, [{ title: newValue.title, value: newValue.value }])
-}
-
 watch(
   () => filterData[props.name],
-  async (newValue, oldValue) => {
-    if (newValue === oldValue || isBoolean(newValue)) return
-    if (isNull(newValue) || isUndefined(newValue) || (isArray(newValue) && newValue.length === 0)) {
-      selectedItemsCache.value = []
-      if (filterConfigCurrent.value.multiple) {
-        selected.value = []
-      } else {
-        selected.value = null
-      }
-      updateFilterSelected(selected.value)
-      if (autoFetched.value === true || isOneOf(props.prefetch, ['hover', 'focus', false])) return
-      autoFetchTimer.value = setTimeout(() => {
-        autoFetch()
-      }, 3000)
+  async (newValueFilterData, oldValueFilterData) => {
+    if (newValueFilterData === oldValueFilterData || isBoolean(newValueFilterData)) return
+    const newValue = newValueFilterData as ModelValueType
+    if (isFirstLoad.value) {
+      isFirstLoad.value = false
+      await checkFirstLoad()
+    }
+    if (isEmpty(newValue)) {
+      resetToEmptyState(newValue)
       return
     }
-    const found = await tryToLoadFromLocalData(newValue)
-    if (found) {
-      selected.value = selectedItemsCache.value.map((item) => ({ title: item.title, value: item.value }))
-      updateFilterSelected(selected.value)
-      return
-    }
-    if (isArray(newValue)) {
-      loading.value = true
-      selectedItemsCache.value = await props.fetchItemsByIds(newValue as Array<IntegerId & DocId>)
-      selected.value = selectedItemsCache.value.map((item) => ({ title: item.title, value: item.value }))
-      updateFilterSelected(selected.value)
-      loading.value = false
-      return
-    }
-    loading.value = true
-    selectedItemsCache.value = await props.fetchItemsByIds([newValue as DocId & IntegerId])
-    selected.value = selectedItemsCache.value.map((item) => ({ title: item.title, value: item.value }))[0]
-    updateFilterSelected(selected.value)
-    loading.value = false
+    await loadListItems(newValue as T[] | T)
   },
   { immediate: true }
-)
-
-watchDebounced(
-  search,
-  (newValue, oldValue) => {
-    if (newValue.length < props.searchMinChars) return
-    if (newValue !== oldValue) {
-      apiRequestCounter.value++
-      apiSearch(newValue, apiRequestCounter.value)
-    }
-  },
-  { debounce: 300 }
 )
 </script>
 
@@ -330,6 +300,7 @@ watchDebounced(
     hide-details
     return-object
     autocomplete="off"
+    :no-data-text="noDataText"
     @update:search="onSearchUpdate"
     @update:model-value="onSelectedUpdate"
     @blur="onBlur"
@@ -349,6 +320,22 @@ watchDebounced(
         :title="item.raw.title"
         :subtitle="item.raw.subtitle"
       />
+    </template>
+    <template #chip="{ props: chipProps, item }">
+      <VChip
+        :closable="chipProps.closable as boolean"
+        size="small"
+        :text="`${item.title} (${item.raw.subtitle})`"
+        :disabled="item.props.disabled"
+      >
+        {{ item.raw.title }}
+        <span
+          v-if="item.raw.subtitle"
+          class="font-italic pl-1"
+        >
+          ({{ item.raw.subtitle }})
+        </span>
+      </VChip>
     </template>
   </VAutocomplete>
 </template>
