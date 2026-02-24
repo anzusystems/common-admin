eb71f45aa30551b4bbeacf56b498b02582c80d9f
volar <stanislav.volar@petitpress.sk>
2026-02-24 11:00:17 +0100

    wip

--- src/components/dam/assetSelect/components/AssetSelectListBar.vue
+++ src/components/dam/assetSelect/components/AssetSelectListBar.vue
@@ -225,6 +225,7 @@ onMounted(() => {
             <VBtn
               size="x-small"
               icon
+              color="light"
               variant="text"
               @click.stop="setGridView(AssetSelectGridView.Masonry)"
             >
@@ -238,6 +239,7 @@ onMounted(() => {
             </VBtn>
             <VBtn
               icon
+              color="light"
               size="x-small"
               variant="text"
               @click.stop="setGridView(AssetSelectGridView.Thumbnail)"
@@ -253,6 +255,7 @@ onMounted(() => {
             <VBtn
               size="x-small"
               icon
+              color="light"
               variant="text"
               @click.stop="setGridView(AssetSelectGridView.Table)"
             >
@@ -270,6 +273,7 @@ onMounted(() => {
             />
             <VBtn
               icon
+              color="light"
               :width="30"
               :height="30"
               :active="sidebarRight"
