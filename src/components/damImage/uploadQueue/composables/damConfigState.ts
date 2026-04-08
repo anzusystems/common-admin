fd23e47a33967c0dd9a47c8d955c2d9f1ac46bc1
volar <stanislav.volar@petitpress.sk>
2026-04-08 12:14:56 +0200

    wip

--- src/components/damImage/uploadQueue/composables/damConfigState.ts
+++ src/components/damImage/uploadQueue/composables/damConfigState.ts
@@ -75,7 +75,6 @@ export function useDamConfigState(client: undefined | (() => AxiosInstance) = un
           if (Object.keys(config).length < 1) {
             throw new Error('Unable to load dam prv config. Incorrect response body.')
           }
-          console.log(config)
           setDamPrvConfig(config)
           resolve(true)
         })
